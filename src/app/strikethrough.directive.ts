596eab840b11a78002bf788569ff030ffa875a29
WendyOmollo <omollow280@gmail.com>
2019-08-15 21:15:31 +0300

    Make a project using angular

---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,18 @@
+import { Directive , ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+
+  constructor(private elem:ElementRef){}
+  @HostListener('click') onClicks(){
+    this.Deco ('line-through')
+  }
+  @HostListener('dblclick') onDoubleClicks(){
+    this.Deco ('None')
+  }
+  private Deco(action:string){
+    this.elem.nativeElement.style.textDecoration = action;
+  }
+}
